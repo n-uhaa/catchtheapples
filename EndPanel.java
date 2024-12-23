c55a5c197c183fc0f59e4e98bcfbb114bcf08558
n-uhaa <Nuuhhaa.s@gmail.com>
2024-12-22 23:14:55 -0500

    Update EndPanel.java

--- EndPanel.java
+++ EndPanel.java
@@ -1,5 +1,3 @@
-//Nuha Samen & Emaan Taswar
-//January 8th 2023
 //Summative Project - catch the apples: A fun game to test your reaction time!
 //end panel - controls end screen, displays score and allows user to restart or go back to main
 
