07d9fea3abd107571e82d6563e30b20886fc514d
n-uhaa <Nuuhhaa.s@gmail.com>
2024-12-22 23:14:21 -0500

    Update InstructionsPanel.java

--- InstructionsPanel.java
+++ InstructionsPanel.java
@@ -1,5 +1,3 @@
-//Nuha Samen & Emaan Taswar
-//January 8th 2023
 //Summative Project - catch the apples: A fun game to test your reaction time!
 //instruction panel - controls the graphics and button on this panel
 
